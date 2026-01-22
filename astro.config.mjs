d88b391db4acb86038133a7795cbcbc78495bd44
kanglam2026 <kanglam2026@gmail.com>
2026-01-22 15:27:25 +0530

    added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
+// CHANGE THESE ⬇️
+const username = "YOUR_GITHUB_USERNAME";
+const repo = "YOUR_REPO_NAME";
+
 export default defineConfig({
+  site: `https://${username}.github.io`,
+  base: `/${repo}/`,
+  output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
